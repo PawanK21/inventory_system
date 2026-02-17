855040ee98c97e9c8533a07d376a05c9e95bee7f
Pawan Kumar <thelimitlesspawankumar@gmail.com>
2026-02-17 20:10:29 +0530

    host updated

--- main.py
+++ main.py
@@ -671,4 +671,4 @@ async def get_stats():
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="localhost", port=8000, reload=False)
